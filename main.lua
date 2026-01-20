73014e8622d0953f517e27b6061b7dff4068a294
parnic <github@parnic.com>
2026-01-20 14:15:48 -0600

    Minimize loc

--- main.lua
+++ main.lua
@@ -22,15 +22,13 @@ f:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
 f:SetWidth(128)
 f:SetHeight(256)
 
-local bar = CreateFrame("StatusBar", "ReverseStatusBarBug_Bar", f)
-bar:SetAllPoints(f)
-bar:SetMinMaxValues(0, 1)
-bar:SetValue(0)
-bar:SetOrientation("VERTICAL")
-bar:SetStatusBarTexture("Interface\\AddOns\\ReverseStatusBarBug\\bar")
+f.bar = CreateFrame("StatusBar", "ReverseStatusBarBug_Bar", f)
+f.bar:SetAllPoints(f)
+f.bar:SetMinMaxValues(0, 1)
+f.bar:SetValue(0)
+f.bar:SetOrientation("VERTICAL")
+f.bar:SetStatusBarTexture("Interface\\AddOns\\ReverseStatusBarBug\\bar")
 -- either of these methods cause the texture to "slide in" rather than "reveal" as the bottom of the texture is always the
 -- coordinate anchor point rather than being relative to the direction of the fill.
-bar:SetReverseFill(true)
--- bar:SetFillStyle(Enum.StatusBarFillStyle.Center)
-
-f.bar = bar
+f.bar:SetReverseFill(true)
+-- f.bar:SetFillStyle(Enum.StatusBarFillStyle.Center)
